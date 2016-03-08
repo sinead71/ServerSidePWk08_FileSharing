c740fcbd0ab8d387fce579022bed4efcf9429005
sinead.71 <cooneysinead71.com>
2016-03-08 15:44:22 +0000

    Creting an upload file page.

--- app.js
+++ app.js
@@ -4,11 +4,35 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
+
+var multer = require('multer');
+
+
+//multer will figure out where the destination is
+var storageMethod = multer.diskStorage({
+    destination: function (req, file, cb) {
+        console.log("In destination");
+        
+        fs.exists('./uploads/', function(exists){
+            if(!exists){
+                fs.mkdir('./uploads/', function(error){
+                    cb(error, './uploads/');
+                })
+            }
+            else {
+                cb(null, './uploads/');
+            }
+        })       
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '_' + file.originalname);
+    }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +46,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/',multer({storage: storageMethod}).any());
 app.use('/', routes);
-app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
+//Temporary global variable
+var files = new Array();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/uploadFile', function(req,res,next){
+    res.render('fileUpload');
+});
+
+router.post('/uploadFile', function(req, res, next){
+    //A file has been uploaded
+    console.log("In POST /uploadFile");
+    console.log(req.files[0].filename);
+    
+    //var theFile = {};
+    //theFile.name = req.files[0].filename;
+    //theFile.id = Date.now();
+    //......or...... 
+    req.files[0]._id = Date.now();
+    console.log(req.files[0]);
+    
+    files.push(req.files[0]); 
+    res.redirect('/files');
+});
+
+router.get('/files', function(req, res, next){
+    res.render('test', {theFiles: files});
+});
+
+router.get('/deleteFile/:fileID', function(req, res, next){
+    console.log("Just be asked to delete " + req.params.fileID);
+    
+    for(var i=0; i < files.length; i++){
+        if(files[i]._id == req.params.fileID){
+            //we have a match
+            fs.unlink('./uploads/' + files[i].filename, function(error){                
+            });
+            
+            files.splice(i,1);
+            break;
+        }    
+    }
+    
+    res.redirect('/files');
 });
 
 module.exports = router;
